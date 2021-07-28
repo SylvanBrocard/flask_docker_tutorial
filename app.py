414456246aec01a39632080c38616192a4ec189a
SylvanBrocard <sylvan.brocard@gmail.com>
2021-07-28 13:50:59 +0200

    taking user input

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-    # return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    # handle the POST request
+    if request.method == 'POST':
+        texte = request.form.get('texte')
+        return '''<h1>Your text was: {}</h1>'''.format(texte)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>Your text is: <input type="text" name="texte"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
